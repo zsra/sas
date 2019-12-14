0a0c71be8860133a79d60ee5b12d7ee528aff3b5
Richard Zsupos <richard.zsupos@gmail.com>
2019-12-14 08:27:32 +0100

    Initial commit with Spring base project and .gitignore

---
+++ enaplo/src/main/java/hu/zsra/enaplo/EnaploApplication.java
@@ -0,0 +1,13 @@
+package hu.zsra.enaplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnaploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnaploApplication.class, args);
+	}
+
+}
---
+++ enaplo/src/test/java/hu/zsra/enaplo/EnaploApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.zsra.enaplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnaploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
